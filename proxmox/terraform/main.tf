6e76afb8f5585268e4e07e825a86a2243a22a711
CharlesJeremy <mulemi.charles29@gmail.com>
2022-08-07 15:14:33 -0700

    Update contoller memory to 4096

--- proxmox/terraform/main.tf
+++ proxmox/terraform/main.tf
@@ -9,7 +9,7 @@ resource "proxmox_vm_qemu" "control_plane" {
   cores             = 4
   sockets           = "1"
   cpu               = "host"
-  memory            = 2048
+  memory            = 4096
   scsihw            = "virtio-scsi-pci"
   bootdisk          = "scsi0"
   kvm               = true
